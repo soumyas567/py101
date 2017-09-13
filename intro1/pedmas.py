2ea1cb602ebfa9d8651475de798b6ea67dadc6ea
soumyasomasundaran <soumyas567@gmail.com>
2017-09-13 18:59:27 +0530

    Pedmas added

---
+++ intro1/pedmas.py
@@ -0,0 +1,26 @@
+#Parenthese
+print 2*(3-1)
+
+print (1+1)**(5-2)
+
+#Exponentiation
+
+print 2**1+1
+ 
+print 3*1**3
+
+print 2*3-1
+
+
+#Multiplication  and Division
+print 6+4/2
+
+
+#Operators with the same precedence are evaluated from left to right
+degrees =30
+pi=3.14
+print degrees/2*pi
+
+
+
+
