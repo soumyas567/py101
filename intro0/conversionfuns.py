8c0f895e63828b14217f7e355ca1bbd74f1425aa
soumyasomasundaran <soumyas567@gmail.com>
2017-09-13 22:56:10 +0530

    new_examples_levelzero

---
+++ intro0/conversionfuns.py
@@ -0,0 +1,5 @@
+print int('23')
+print float(4)
+d=90
+print str(d)
+print type(str(d))#checking the typeof d
---
+++ intro0/mathfun.py
@@ -0,0 +1,18 @@
+import math #creates an  module object math
+log=math.log10(100)
+print log #checking log
+
+angle=math.sin(90)
+
+print angle #checking sin
+
+sqr=math.sqrt(81)
+print sqr #checking sin
+
+
+compo=math.exp(math.sqrt(4)) # exponential give the e^x value
+
+print compo
+
+
+
